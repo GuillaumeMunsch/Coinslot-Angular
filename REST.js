d2b7d648ba3fe92920a6e38358f21cdd38788ef1
Guillaume Munsch <guigui.munsch@gmail.com>
2016-03-05 16:32:52 -0800

    Working

--- REST.js
+++ REST.js
@@ -1,12 +1,25 @@
-function REST_ROUTER(router,connection,md5) {
+var mysql = require("mysql");
+function REST_ROUTER(router, connection, md5) {
     var self = this;
-    self.handleRoutes(router,connection,md5);
+    self.handleRoutes(router, connection, md5);
 }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
-    router.get("/",function(req,res){
-        res.json({"Message" : "Hello World !"});
-    })
+
+    //Get Coinslots
+    router.get("/coinslot",function(req,res){
+        var query = "SELECT * FROM ?? ";
+        var table = ["coinslot"];
+        query = mysql.format(query,table);
+        connection.query(query, function(err, rows){
+            if(err) {
+                res.json({"error" : true, "message" : "Error executing MySQL query"});
+            } else {
+                res.json({"error" : false, "message" : "success", "coinslots" : rows});
+            }
+        });
+    });
+
 }
 
 module.exports = REST_ROUTER;

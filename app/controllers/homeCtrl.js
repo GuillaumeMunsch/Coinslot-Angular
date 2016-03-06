c10bb752bcaf39f01ec914ffb526102da6cfe613
David Henner <david.henner@epitech.eu>
2016-03-05 17:50:42 -0800

    Munsch est bô

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,3 +1,13 @@
-app.controller('homeController', ['$scope', function($scope) {
+app.controller('homeController', ['$http', '$scope', function($http, $scope) {
+
+  var apiPath = "http://192.168.1.10:3000/api/";
+
+  $http.get(apiPath + '/coinslot').
+    success(function(data) {
+      $scope.coinslots = data.coinslots;
+    }).
+    error(function(data) {
+      console.log("Error on performing GET on /coinslots");
+    });
 
   }]);
